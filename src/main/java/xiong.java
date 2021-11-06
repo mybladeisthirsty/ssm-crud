682d3c1ab4e7a4c8468ccf49dcd576cdaabebb4e
myblade <xiongfangxiongfang@163.com>
2021-11-06 13:47:37 +0800

    first commit

---
+++ src/main/java/xiong.java
@@ -0,0 +1,11 @@
+import org.springframework.jdbc.support.incrementer.PostgreSQLSequenceMaxValueIncrementer;
+
+/**
+ * @author xiong
+ * @create 2021-11-06-13:37
+ */
+public class xiong {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
